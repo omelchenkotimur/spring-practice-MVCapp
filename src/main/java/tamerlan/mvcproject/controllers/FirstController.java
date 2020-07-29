3df4c442d2a9706e63250b5a55fa1ed67f606589
Tamerlan <omelchenkotamer@gmail.com>
2020-07-29 16:44:06 +0300

    Java definition page added.

--- src/main/java/tamerlan/mvcproject/controllers/FirstController.java
+++ src/main/java/tamerlan/mvcproject/controllers/FirstController.java
@@ -6,13 +6,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
 
-    @GetMapping("/hello")
-    public  String helloPage(){
-        return "first/hello";
+    @GetMapping("/java")
+    public String java() {
+        return "first/java";
     }
 
-    @GetMapping("/goodbye")
-    public String goodbyePage(){
-        return "first/goodbye";
+    @GetMapping("/general-purpose")
+    public String generalPurpose() {
+        return "first/general-purpose";
+    }
+
+    @GetMapping("/Programming_language")
+    public String programmingLanguage(){
+        return "first/programming_language";
+    }
+
+    @GetMapping("/Class-based_programming")
+    public String classBasedProgramming() {
+        return "first/class-based_programming";
+    }
+
+    @GetMapping("/Object-oriented_programming")
+    public String objectOrientedProgramming() {
+        return "first/object-oriented_programming";
+    }
+
+    @GetMapping("/coupling")
+    public String coupling() {
+        return "first/coupling";
     }
 }
