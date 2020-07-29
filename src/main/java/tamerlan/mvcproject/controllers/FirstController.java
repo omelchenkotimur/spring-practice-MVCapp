4cbf59b0bba6df5f79d52e4ae11c40507b12c14c
Tamerlan <omelchenkotamer@gmail.com>
2020-07-29 12:35:07 +0300

    First & Second controllers added.

---
+++ src/main/java/tamerlan/mvcproject/controllers/FirstController.java
@@ -0,0 +1,18 @@
+package tamerlan.mvcproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public  String helloPage(){
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage(){
+        return "first/goodbye";
+    }
+}
