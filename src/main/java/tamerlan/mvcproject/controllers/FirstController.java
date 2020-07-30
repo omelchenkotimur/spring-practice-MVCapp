af1faafd66bafd86b0038fb2f589a9c7a77720da
Tamerlan <omelchenkotamer@gmail.com>
2020-07-30 13:41:50 +0300

    Data transfer from URL added.

--- src/main/java/tamerlan/mvcproject/controllers/FirstController.java
+++ src/main/java/tamerlan/mvcproject/controllers/FirstController.java
@@ -2,6 +2,9 @@ package tamerlan.mvcproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
@@ -34,5 +37,24 @@ public class FirstController {
     @GetMapping("/coupling")
     public String coupling() {
         return "first/coupling";
+    }
+
+    @GetMapping("/hello")
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello " + name + " " + surname);
+
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage(@RequestParam (value = "name", required = false)  String name,
+                              @RequestParam (value = "surname", required = false) String surname){
+
+        System.out.println("Goodbye " + name + " " + surname);
+
+        return "first/goodbye";
     }
 }
